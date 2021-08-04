df4ad7130324763420c8e5ddf9ca159039df5ad4
tdean41 <timothy.edward.dean@gmail.com>
2021-08-04 15:42:29 -0400

    string replace

--- DensityMatch.py
+++ DensityMatch.py
@@ -16,6 +16,7 @@ def archive_data(csv):
 	archive_data = pd.read_csv(csv, parse_dates=['PatientsBirthDate'])
 	archive_data['StudyDate'] = archive_data['StudyDate'].astype(str)
 	archive_data['PatientsBirthDate'] = archive_data['PatientsBirthDate'].astype(str)
+	archive_data['PatientId'] = archive_data['PatientId'].str.replace("_FIX1_", "")
 	archive_data['a_concat_1'] = archive_data['PatientsName'] + archive_data['PatientsBirthDate'] + archive_data['StudyDate']
 	archive_data['a_concat_2'] = archive_data['PatientId'] + archive_data['PatientsBirthDate'] + archive_data['StudyDate']
 	return archive_data
@@ -29,7 +30,7 @@ def pink_data(csv):
 	pink_data = pd.read_csv(csv, parse_dates=['DOB'])
 	pink_data['DOB'] = pink_data['DOB'].astype(str)
 	pink_data['examdate'] = pink_data['examdate'].astype(str)
-	pink_data['p_concat_1'] = pink_data['FName'] + pink_data['LName'] + pink_data['DOB'] + pink_data['DOB']
+	pink_data['p_concat_1'] =  pink_data['LName'] + "^" + pink_data['FName'] + pink_data['DOB'] + pink_data['DOB']
 	pink_data['p_concat_2'] = pink_data['mrn'] + pink_data['DOB'] + pink_data['examdate']
 	return pink_data
 
@@ -42,6 +43,13 @@ def show_density(df, csv):
 	graph = sns.barplot(x = df.index, y = 'mrn', data=df)
 	graph = plt.show()
 	return graph
+
+pink = pink_data(pink_csv)
+archive = archive_data(archive_csv)
+
+a = pd.concat([pink, archive], axis=0, ignore_index=True)
+
+print(test)
 
 #pink = pink_data(pink_csv)
 
